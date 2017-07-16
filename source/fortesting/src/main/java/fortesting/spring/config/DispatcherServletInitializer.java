5a7172628788f29ba36d157ba80ca1fdc7004dfd
DennisD <dses5900@gmail.com>
2017-07-16 13:29:44 +0800

    change to hibernate with JNDI

--- source/fortesting/src/main/java/fortesting/spring/config/DispatcherServletInitializer.java
+++ source/fortesting/src/main/java/fortesting/spring/config/DispatcherServletInitializer.java
@@ -11,7 +11,7 @@ public class DispatcherServletInitializer extends AbstractAnnotationConfigDispat
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[]{JavaConfig.class};
+        return new Class[]{ServletConfig.class};
     }
 
     @Override
